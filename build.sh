aad69f7de2ec3dc5cb89c632a2fb656295dd2466
Boris Rybalkin <ribalkin@gmail.com>
2017-04-01 12:34:56 +0100

    no more libs

--- build.sh
+++ build.sh
@@ -40,7 +40,6 @@ cp -r ${DIR}/bin ${BUILD_DIR}
 cp -r ${DIR}/hooks ${BUILD_DIR}
 cp -r ${DIR}/templates ${BUILD_DIR}
 cp -r ${DIR}/www ${BUILD_DIR}
-cp -r ${DIR}/lib ${BUILD_DIR}
 
 mkdir ${BUILD_DIR}/META
 echo ${NAME} > ${BUILD_DIR}/META/app
--- deps.sh
+++ deps.sh
@@ -8,7 +8,7 @@ pip install coin
 ARCH=$(uname -m)
 if [ $ARCH == "x86_64" ]; then
   wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 --progress dot:giga
-  tar xjvf phantomjs-2.1.1-linux-x86_64.tar.bz2
+  tar xjf phantomjs-2.1.1-linux-x86_64.tar.bz2
   cp ./phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/bin
 else
   wget https://github.com/fg2it/phantomjs-on-raspberry/releases/download/v2.1.1-wheezy-jessie-armv6/phantomjs --progress dot:giga
