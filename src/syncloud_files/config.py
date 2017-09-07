9e0117647fbc6146a3a703d6cfda9cf10013d00e
Boris Rybalkin <ribalkin@gmail.com>
2017-09-07 08:06:16 +0100

    fix data dir

--- src/syncloud_files/config.py
+++ src/syncloud_files/config.py
@@ -8,7 +8,7 @@ class BrowserConfig:
 
     def __init__(self, data_dir):
         self.data_dir = data_dir
-        self.config_dir = join(dara_dir, 'config')
+        self.config_dir = join(data_dir, 'config')
         self.parser = ConfigParser()
         self.filename = join(self.config_dir, BROWSER_CONFIG_NAME)
         self.parser.read(self.filename)
