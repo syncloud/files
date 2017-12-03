933a5b955b8b4cd77173c59e29fbc88b5d92a807
Boris Rybalkin <ribalkin@gmail.com>
2017-12-03 15:28:33 +0000

    fix nginx wrapper

--- bin/service.nginx.sh
+++ bin/service.nginx.sh
@@ -10,19 +10,19 @@ fi
 case $1 in
 pre-start)
     /bin/rm -f ${SNAP_COMMON}/web.socket
-    exec $DIR/nginx/sbin/nginx -t -c ${SNAP_COMMON}/config/nginx/nginx.conf -p $DIR/nginx -g \'error_log ${SNAP_COMMON}/log/nginx_error.log warn;\'
+    exec ${DIR}/nginx/sbin/nginx -t -c ${SNAP_COMMON}/config/nginx/nginx.conf -p ${DIR}/nginx -g 'error_log '${SNAP_COMMON}'/log/nginx_error.log warn;'
     ;;
 start)
-    exec $DIR/nginx/sbin/nginx -c ${SNAP_COMMON}/config/nginx/nginx.conf -p $DIR/nginx -g \'error_log ${SNAP_COMMON}/log/nginx_error.log warn;\'
+    exec ${DIR}/nginx/sbin/nginx -c ${SNAP_COMMON}/config/nginx/nginx.conf -p ${DIR}/nginx -g 'error_log '${SNAP_COMMON}'/log/nginx_error.log warn;'
     ;;
 post-start)
-    timeout 5 /bin/bash -c 'until [ -f ${SNAP_COMMON}/web.socket ]; do sleep 1; done'
+    timeout 5 /bin/bash -c 'until [ -S '${SNAP_COMMON}'/web.socket ]; do echo "waiting for ${SNAP_COMMON}/web.socket"; sleep 1; done'
     ;;
 reload)
-    $DIR/nginx/sbin/nginx -c ${SNAP_COMMON}/config/nginx/nginx.conf -s reload -p $DIR/nginx
+    ${DIR}/nginx/sbin/nginx -c ${SNAP_COMMON}/config/nginx/nginx.conf -s reload -p ${DIR}/nginx
     ;;
 stop)
-    $DIR/nginx/sbin/nginx -c ${SNAP_COMMON}/config/nginx/nginx.conf -s stop -p $DIR/nginx
+    ${DIR}/nginx/sbin/nginx -c ${SNAP_COMMON}/config/nginx/nginx.conf -s stop -p ${DIR}/nginx
     ;;
 *)
     echo "not valid command"
