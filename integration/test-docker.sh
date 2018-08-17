c8e3c44ded69580baf9876de180c3418f8b6a29c
Boris Rybalkin <ribalkin@gmail.com>
2018-08-17 08:14:02 +0100

    firefox comes in build image

--- integration/test-docker.sh
+++ integration/test-docker.sh
@@ -59,10 +59,6 @@ sshpass -p syncloud ssh -o StrictHostKeyChecking=no root@${DEVICE_HOST} /install
 pip2 install -r ${DIR}/../src/dev_requirements.txt
 pip2 install -r ${DIR}/../requirements.txt
 
-coin --to ${DIR} raw --subfolder geckodriver https://github.com/mozilla/geckodriver/releases/download/v${GECKODRIVER}/geckodriver-v${GECKODRIVER}-linux64.tar.gz
-coin --to ${DIR} raw https://ftp.mozilla.org/pub/firefox/releases/${FIREFOX}/linux-x86_64/en-US/firefox-${FIREFOX}.tar.bz2
-curl https://raw.githubusercontent.com/mguillem/JSErrorCollector/master/dist/JSErrorCollector.xpi -o  JSErrorCollector.xpi
-
 #fix dns
 device_ip=$(getent hosts ${DEVICE_HOST} | awk '{ print $1 }')
 echo "$device_ip $DOMAIN.syncloud.info" >> /etc/hosts
